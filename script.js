e0bbd00826072232dc083277f75320f4620be018
Jessica Huang <jessicachieh.huang@mail.utoronto.ca>
2026-03-16 13:54:18 -0400

    completed step 2-3

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ Step 1: INITIALIZE MAP
 --------------------------------------------------------------------*/
 // Define access token
 mapboxgl.accessToken =
-  "pk.eyJ1IjoiamVzc2ljYWh1YW5nIiwiYSI6ImNtazNjNmdmeTBkN3AzZnEyZHRscHdod28ifQ.Pa9LhzBk1H75KBMwBngDjA"; //****ADD YOUR PUBLIC ACCESS TOKEN*****
+  "pk.eyJ1IjoiamVzc2ljYWh1YW5nIiwiYSI6ImNtazNjNmdmeTBkN3AzZnEyZHRscHdod28ifQ.Pa9LhzBk1H75KBMwBngDjA";
+//****ADD YOUR PUBLIC ACCESS TOKEN*****
 
 // Initialize map and edit to your preference
 const map = new mapboxgl.Map({
@@ -33,15 +34,72 @@ fetch(
   .then((response) => {
     console.log(response);
     collision_point = response;
-    let envresult = turf.envelope(collision_point);
-    console.log(envresult.bbox);
+    map.on("load", () => {
+      const bbox = turf.bbox(collision_point);
+      console.log("bbox:", bbox);
 
-    bboxgeojson = {
-      type: "Feature Collection",
-      features: [envresult],
-    };
+      // convert bbox array to polygon feature
+      const bboxPolygon = turf.bboxPolygon(bbox);
+      console.log("bbox polygon:", bboxPolygon);
 
-    console.log(bboxgeojson.features[0].geometry.coordinates[0][0][1]);
+      // scale bbox polygon by 10%
+      const scaledBboxPolygon = turf.transformScale(bboxPolygon, 1.1);
+      console.log("scaled bbox polygon:", scaledBboxPolygon);
+
+      // convert scaled polygon back to bbox array
+      const scaledBbox = turf.bbox(scaledBboxPolygon);
+      console.log("scaled bbox:", scaledBbox);
+
+      const hexgrid = turf.hexGrid(scaledBbox, 0.5);
+      console.log(hexgrid);
+
+      const collishex = turf.collect(hexgrid, collision_point, "_id", "values");
+      console.log(collishex);
+
+      let maxcollis = 0;
+      collishex.features.forEach((feature) => {
+        feature.properties.COUNT = feature.properties.values.length;
+        if (feature.properties.COUNT > maxcollis) {
+          console.log(feature);
+          maxcollis = feature.properties.COUNT;
+        }
+      });
+      console.log(maxcollis);
+
+      // Add datasource using GeoJSON variable
+
+      map.addSource("input-data", {
+        type: "geojson",
+        data: "https://raw.githubusercontent.com/JessicaCHuang26/ggr472-lab4-main/refs/heads/main/data/pedcyc_collision_06-21.geojson",
+      });
+
+      map.addSource("hexgrid", {
+        type: "geojson",
+        data: hexgrid,
+      });
+
+      // Set style for when new points are added to the data source
+      map.addLayer({
+        id: "input-pnts",
+        type: "circle",
+        source: "input-data",
+        paint: {
+          "circle-radius": 5,
+          "circle-color": "blue",
+        },
+      });
+
+      map.addLayer({
+        id: "hexgrid-fill",
+        type: "fill",
+        source: "hexgrid",
+        paint: {
+          "fill-color": "#62d1ea",
+          "fill-opacity": 0.3,
+          "fill-outline-color": "#000000",
+        },
+      });
+    });
   });
 
 /*--------------------------------------------------------------------
@@ -53,25 +111,6 @@ fetch(
 //      Use bounding box coordinates as argument in the turf hexgrid function
 //      **Option: You may want to consider how to increase the size of your bbox to enable greater geog coverage of your hexgrid
 //                Consider return types from different turf functions and required argument types carefully here
-
-map.on("load", () => {
-  // Add datasource using GeoJSON variable
-  map.addSource("input-data", {
-    type: "geojson",
-    data: "https://raw.githubusercontent.com/JessicaCHuang26/ggr472-lab4-main/refs/heads/main/data/pedcyc_collision_06-21.geojson",
-  });
-
-  // Set style for when new points are added to the data source
-  map.addLayer({
-    id: "input-pnts",
-    type: "circle",
-    source: "input-data",
-    paint: {
-      "circle-radius": 5,
-      "circle-color": "blue",
-    },
-  });
-});
 
 /*--------------------------------------------------------------------
 Step 4: AGGREGATE COLLISIONS BY HEXGRID
